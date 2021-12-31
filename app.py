5a2df7a496b506e6e17ac5dea3c46e28db666245
Tzofnat Cohen <ex_tzofnat_tperson@gethomesome.com>
2021-12-31 14:00:00 +0200

    TournamentsCreate

--- app.py
+++ app.py
@@ -10,13 +10,10 @@ app.config.from_pyfile('settings.py')
 from pages.homepage.homepage import homepage
 app.register_blueprint(homepage)
 
-## About
-from pages.about.about import about
-app.register_blueprint(about)
 
 ## Catalog
-from pages.catalog.catalog import catalog
-app.register_blueprint(catalog)
+from pages.TournamentsCreate.TournamentsCreate import TournamentsCreate
+app.register_blueprint(TournamentsCreate)
 
 ## Page error handlers
 from pages.page_error_handlers.page_error_handlers import page_error_handlers
@@ -27,7 +24,3 @@ app.register_blueprint(page_error_handlers)
 ## Main menu
 from components.main_menu.main_menu import main_menu
 app.register_blueprint(main_menu)
-
-## footer
-from components.footer.footer import footer
-app.register_blueprint(footer)
